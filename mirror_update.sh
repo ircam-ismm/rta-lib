f75eb5837d719b8d40867f63a4e87d6dc65726b8
Jean-Philippe Lambert <Jean-Philippe.Lambert@ircam.fr>
2008-05-22 14:42:32 +0000

    added Doxyfile for mirroring

--- mirror_update.sh
+++ mirror_update.sh
@@ -8,7 +8,7 @@
 mirror_dir=rta_mirror # a symbolic link is ok
 
 
-for source in $( ls *.{c,h} matlab/*.{c,h,m} doc/README.txt documentation/*.{tex,pdf,graffle} ); do
+for source in $( ls Doxyfile *.{c,h} matlab/*.{c,h,m} doc/README.txt documentation/*.{tex,pdf,graffle} ); do
 #    diff ${source}  ${mirror_dir}/${source}
     cat ${source} > ${mirror_dir}/${source}
 done
