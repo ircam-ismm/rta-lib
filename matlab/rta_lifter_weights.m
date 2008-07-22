d7561d3d9e8bdd2997f9b4ef5138132a876396fd
Jean-Philippe Lambert <Jean-Philippe.Lambert@ircam.fr>
2008-07-22 11:01:32 +0000

    added liftering function

---
+++ matlab/rta_lifter_weights.m
@@ -0,0 +1,18 @@
+function [weights] = rta_lifter_weights (size, coefficient, type, mode)
+% function [weights] = rta_lifter_weights (size, type, coefficient)
+%
+% Create a vector (a <size> x 1 matrix) of <weights>. Only the <size>
+% parameter is mandatory.
+%
+% <type> can be 'slaney' (default, same as 'exp') or 'htk' (same as
+%    'sin').
+% <coefficient> is optional (0.6 by default, common for 'exp').
+%    It must be > 0 for 'sin' (22 is common).
+% <mode> can be 'normal' (default) or 'inverse'
+%
+
+['This file is an help file which relies on a mex file of the same' ...
+ ' name (but with a different extension).\n']
+%
+% 2008 (C) Ircam - Centre Pompidou
+% Jean-Philippe.Lambert@ircam.fr
