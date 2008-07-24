9670eb6f07a189f4518e306080ac4f06afe0cfef
Jean-Philippe Lambert <Jean-Philippe.Lambert@ircam.fr>
2008-07-24 17:35:58 +0000

    doc update

--- matlab/rta_lifter_weights.m
+++ matlab/rta_lifter_weights.m
@@ -4,10 +4,10 @@ function [weights] = rta_lifter_weights (size, coefficient, type, mode)
 % Create a vector (a <size> x 1 matrix) of <weights>. Only the <size>
 % parameter is mandatory.
 %
-% <type> can be 'slaney' (default, same as 'exp') or 'htk' (same as
-%    'sin').
 % <coefficient> is optional (0.6 by default, common for 'exp').
 %    It must be > 0 for 'sin' (22 is common).
+% <type> can be 'slaney' (default, same as 'exp') or 'htk' (same as
+%    'sin').
 % <mode> can be 'normal' (default) or 'inverse'
 %
 
