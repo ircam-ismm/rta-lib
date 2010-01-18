56a9ba56e479089c5a48a0ac71b05dc522ec2382
Diemo Schwarz <Diemo.Schwarz@ircam.fr>
2010-01-18 16:59:51 +0000

    disco file functions in separate source

---
+++ distlib/discofile.h
@@ -0,0 +1,50 @@
+
+/*
+ *	DISCO descriptor file format support
+ */
+
+#ifndef _DISCOFILE_H_
+#define _DISCOFILE_H_
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* header (12 bytes):
+   int32 ndata		number of vectors
+   int32 ndim		number of elements of each vector
+   int32 descrid	descriptor ID
+
+   data: ndata * ndim float32 values
+ */
+
+typedef struct _disco_file_header
+{
+    int 	ndata;		/* number of vectors */
+    int 	ndim;		/* number of elements of each vector */
+    int 	descrid;	/* descriptor ID */
+    float	data[0];	/* ndata * ndim float32 values */
+} disco_file_header_t;
+
+
+typedef struct _disco_file
+{
+    int		fd;		/* file descriptor */
+    size_t	len;		/* mapped length in bytes */
+    const char *filename;	/* file name opened */
+    disco_file_header_t *base;
+} disco_file_t;
+
+
+
+void *disco_file_map (disco_file_t *file, const char *name, int nvec);
+void disco_file_unmap (disco_file_t *file);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _DISCOFILE_H_ */
