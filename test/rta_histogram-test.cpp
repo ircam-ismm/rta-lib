de45fd5a2b587b32d8625fcd6a401420bd5decec
diemo <diemo.schwarz@ircam.fr>
2022-09-21 15:03:44 +0200

    add weighted histogram test with edge values

--- test/rta_histogram-test.cpp
+++ test/rta_histogram-test.cpp
@@ -1,4 +1,4 @@
-
+// unit test for rta histogram using catch v1 framework (run by pipo test target)
 #include "catch.hpp"
 #include "rta_histogram.h"
 
@@ -7,26 +7,49 @@ TEST_CASE("rta_histogram")
   rta_histogram_params_t hist;
   rta_histogram_init(&hist);
 
-  SECTION("Data")
-  {
-    rta_real_t data[] = {2, 3, 3, 4, 4, 4};
-    int size = sizeof(data) / sizeof(*data);
+  rta_real_t data[] = {2, 3, 3, 4, 4, 4};
+  int size = sizeof(data) / sizeof(*data);
     
-    hist.nhist = 3;
-    rta_real_t output[hist.nhist];
-    rta_real_t binout[hist.nhist];
+  hist.nhist = 3;
+  rta_real_t output[hist.nhist];
+  rta_real_t binout[hist.nhist];
 
+  SECTION("Unweighted")
+  {
     rta_histogram_stride(&hist, data, 1, size, output, 1, binout, 1);
 
     // verify return bin x coordinates
-    CHECK((float) binout[0] == 2);
-    CHECK((float) binout[1] == 3);
-    CHECK((float) binout[2] == 4);
+    CHECK(binout[0] == 2);
+    CHECK(binout[1] == 3);
+    CHECK(binout[2] == 4);
 
     // verify returned counts
-    CHECK((float) output[0] == 1);
-    CHECK((float) output[1] == 2);
-    CHECK((float) output[2] == 3);
+    CHECK(output[0] == 1);
+    CHECK(output[1] == 2);
+    CHECK(output[2] == 3);
+
+    CHECK(hist.lo == 2);
+    CHECK(hist.hi == 4);
+  }
+
+  SECTION("Weighted")
+  {
+    rta_real_t weights[] = {1, 1, 1, 0, 0, 0};
+    
+    rta_histogram_weighted_stride(&hist, data, 1, size, weights, 1, output, 1, binout, 1);
+
+    // verify return bin x coordinates
+    CHECK(binout[0] == 2);
+    CHECK(binout[1] == 3);
+    CHECK(binout[2] == 4);
+
+    // verify returned counts
+    CHECK(output[0] == 1);
+    CHECK(output[1] == 2);
+    CHECK(output[2] == 0);
+
+    CHECK(hist.lo == 2);
+    CHECK(hist.hi == 4);
   }
 }
 
